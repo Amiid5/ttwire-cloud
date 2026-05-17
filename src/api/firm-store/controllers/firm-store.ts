b3be9e636678bee3acf90e3724c40abb7f5c2fd4
Mohamed Amin <amenahmed667788@gmail.com>
2026-05-17 14:56:48 +0300

    added new cont types

---
+++ src/api/firm-store/controllers/firm-store.ts
@@ -0,0 +1,7 @@
+/**
+ * firm-store controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::firm-store.firm-store');
---
+++ src/api/firm-store/routes/firm-store.ts
@@ -0,0 +1,7 @@
+/**
+ * firm-store router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::firm-store.firm-store');
